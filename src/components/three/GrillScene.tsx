09d9303c2a715162f96e4a9034c5d66a076a7f29
spewite <jiz.j12.a4@gmail.com>
2026-03-12 21:23:40 +0100

    feat: improve 3D model camera

--- src/components/three/GrillScene.tsx
+++ src/components/three/GrillScene.tsx
@@ -23,21 +23,20 @@ interface GrillSceneProps {
 
 export default function GrillScene({ grillState }: GrillSceneProps) {
   return (
-    <div className="w-full h-[400px] bg-white rounded-xl shadow-inner border border-gray-100 overflow-hidden relative">
+    <div className="w-full h-[250px] bg-white rounded-xl shadow-inner border border-gray-100 overflow-hidden relative">
       <Canvas
         shadows
-        camera={{ position: [5, 3, 5], fov: 45 }}
+        camera={{ position: [0, 2, 11], fov: 23 }}
         gl={{ antialias: true, alpha: true }}
       >
         <Suspense fallback={<Loader />}>
-          {/* Corregido: contactShadow espera un objeto de configuración o booleano según versión */}
+        
           <Stage 
             environment="city" 
             intensity={0.5} 
-            adjustCamera={true}
+            adjustCamera={false}
           >
-            <Center>
-              {/* Ahora acepta 'scale' porque lo definimos en la interface de GrillModel */}
+            <Center top>
               <GrillModel grillState={grillState} scale={1} />
             </Center>
           </Stage>
@@ -45,9 +44,9 @@ export default function GrillScene({ grillState }: GrillSceneProps) {
           <OrbitControls 
             enablePan={false} 
             minPolarAngle={Math.PI / 2.2} 
-            maxPolarAngle={Math.PI / 2.2}
-            minAzimuthAngle={-1.309}
-            maxAzimuthAngle={1.309}
+            maxPolarAngle={Math.PI / 2}
+            minAzimuthAngle={-Math.PI / 12}
+            maxAzimuthAngle={Math.PI / 6}
             enableZoom={true}
             autoRotate={false}
           />
@@ -59,12 +58,13 @@ export default function GrillScene({ grillState }: GrillSceneProps) {
             blur={2} 
             far={4.5} 
           />
+          
         </Suspense>
       </Canvas>
       
-      <div className="absolute bottom-4 right-4 bg-white/80 backdrop-blur-xs px-3 py-1 rounded-full text-[10px] text-gray-500 pointer-events-none">
+      {/* <div className="absolute bottom-4 right-4 bg-white/80 backdrop-blur-xs px-3 py-1 rounded-full text-[10px] text-gray-500 pointer-events-none">
         Manten click para rotar · Scroll para zoom
-      </div>
+      </div> */}
     </div>
   )
 }
